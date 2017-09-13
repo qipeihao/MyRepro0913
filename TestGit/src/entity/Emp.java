081af3dafd4ecca7ede563fb32fceb82d67eeff8
qph <qipeihao@foxmail.com>
2017-09-13 09:23:29 +0800

    添加 员工 编号和员工姓名属性

--- TestGit/src/entity/Emp.java
+++ TestGit/src/entity/Emp.java
@@ -1,5 +1,23 @@
 package entity;
 
 public class Emp {
+	private int empNo;
+	private String empName;
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
 
 }
